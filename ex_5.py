7c66cc79b5fe74ebd8f4dc8af58d9527ca104185
Wojtek <layvin@interia.pl>
2023-04-24 13:03:33 +0200

    Fixed ex_5

--- ex_5.py
+++ ex_5.py
@@ -4,3 +4,4 @@ def nawiasy(tekst: str) -> [int, int]:
     liczba_nawiasow_otwierajacych = tekst.count("(") + tekst.count("{") + tekst.count("[")
     liczba_nawiasow_zamykajacych = tekst.count(")") + tekst.count("}") + tekst.count("]")
     return [liczba_nawiasow_otwierajacych, liczba_nawiasow_zamykajacych]
+
