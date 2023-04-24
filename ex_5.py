3d28f0681393fc022b618b40acbb5c40d25822f0
Wojtek <layvin@interia.pl>
2023-04-24 13:02:23 +0200

    Fixed ex_5

--- ex_5.py
+++ ex_5.py
@@ -1,6 +1,6 @@
 # Oblicz liczbę nawiasów w zmiennej, zwroc: [otwierajace, zamykajace]
 
 def nawiasy(tekst: str) -> [int, int]:
-    liczba_nawiasow_otwierajacych = tekst.count("(")
-    liczba_nawiasow_zamykajacych = tekst.count(")")
+    liczba_nawiasow_otwierajacych = tekst.count("(") + tekst.count("{") + tekst.count("[")
+    liczba_nawiasow_zamykajacych = tekst.count(")") + tekst.count("}") + tekst.count("]")
     return [liczba_nawiasow_otwierajacych, liczba_nawiasow_zamykajacych]
